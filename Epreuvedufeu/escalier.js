f7660b94c08031e6ac4269e951e05a3d98c921d8
imvne <imanebeaufils19@gmail.com>
2021-11-24 17:14:25 +0100

    pseudocode

--- Epreuvedufeu/escalier.js
+++ Epreuvedufeu/escalier.js
@@ -1,1 +1,30 @@
-echo ("hello");
+var chiffre = 4, espace = " ", hashtag = "#";
+
+//prompt("Donne moi un chiffre");
+
+echo (chiffre)
+
+
+/* entrée d'un chiffre N 4
+N - 1 = nombre d'espaces1 (3)
+hashtag * 1 = nb hashtag1 (1)
+afficher nb d'espaces1 + nb hashtag1 puis entrer
+
+nombre d'espaces1 (3) - 1 = nombre d'espaces2 (2)
+nb hashtag1 (1) + 1 = nb hashtag2 (2)
+afficher nb d'espaces2 + nb hashtag2 puis entrer
+
+nombre d'espaces2 (2) - 1 = nombre d'espaces3 (1)
+nb hashtag2 (2) + 1 = nb hashtag3 (3)
+afficher nb d'espaces3 + nb hashtag3 puis entrer
+
+nombre d'espaces3 (1) - 1 = nombre d'espaces4 (0)
+nb hashtag3 (3) + 1 = nb hashtag4 (4)
+afficher nb d'espaces4 + nb hashtag4 puis entrer
+
+
+   #
+  ##
+ ###
+####
+*/
