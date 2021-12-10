776c2a8e896635375f2a654b0fdeafdfc5e65e83
imvne <imanebeaufils19@gmail.com>
2021-12-10 16:55:56 +0100

    ajout algorithme

--- Epreuvedufeu/escalier.js
+++ Epreuvedufeu/escalier.js
@@ -1,11 +1,35 @@
-var chiffre = 4, espace = " ", hashtag = "#";
+//var chiffre = 4, espace = " ", hashtag = "#";
 
 //prompt("Donne moi un chiffre");
 
-echo (chiffre)
+//echo (chiffre)
 
 
-/* entrée d'un chiffre N 4
+const argument1 = process.argv[2];
+
+var espace, hashtag, lescalier;
+
+function escalier(chiffre) {
+  
+  if (isNaN(chiffre)) {
+    console.log("Put a number");
+  }
+  
+  for (var i = 1; i <= chiffre; i++) {
+        espace = chiffre - i;
+        hashtag = i;
+        lescalier = " ".repeat(espace) + "#".repeat (hashtag);
+
+    console.log(lescalier)
+  }
+  
+}
+
+escalier(argument1);
+
+
+
+/* entrée d'un chiffre N = 4
 N - 1 = nombre d'espaces1 (3)
 hashtag * 1 = nb hashtag1 (1)
 afficher nb d'espaces1 + nb hashtag1 puis entrer
