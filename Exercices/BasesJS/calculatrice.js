e9fe53b3343d50ee2990b4ca55e263df73616c12
imvne <imanebeaufils19@gmail.com>
2022-01-26 11:07:57 +0100

    calc finie

--- Exercices/BasesJS/calculatrice.js
+++ Exercices/BasesJS/calculatrice.js
@@ -1,33 +1,16 @@
-let total = 0;
+const buttons = document.querySelectorAll(".btn");
+const result = document.getElementById("result");
 
-function addition(x) {
-  total += x;
-  return total;
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    result.textContent += e.target.id;
+  });
+});
 
-function soustraction(x) {
-  total -= x;
-  return total;
-}
+equal.addEventListener("click", () => {
+  result.textContent = eval(result.textContent);
+});
 
-function division(x) {
-  if (total === 0) {
-    return (total = x);
-  } else {
-    total /= x;
-    return total;
-  }
-}
-
-function multiplication(x) {
-  if (total === 0) {
-    return (total = x);
-  } else {
-    total *= x;
-    return total;
-  }
-}
-
-function reset() {
-  total = 0;
-}
+clear.addEventListener("click", () => {
+  result.textContent = "";
+});
